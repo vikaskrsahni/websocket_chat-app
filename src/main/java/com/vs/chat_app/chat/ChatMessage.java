3e9b5369d045797f7197ebfdd4605ef97429fe34
Vikas kumar <125907920+vikaskrsahni@users.noreply.github.com>
2025-01-15 00:17:39 +0530

    Add files via upload
    Initial commit

---
+++ src/main/java/com/vs/chat_app/chat/ChatMessage.java
@@ -0,0 +1,16 @@
+package com.vs.chat_app.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private MessageType type;
+    private String content;
+    private String sender;
+
+}
